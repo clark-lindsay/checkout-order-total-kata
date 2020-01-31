12e6d8d6f22d8c86f5d3c17f6fb4967b37d5f805
clark-lindsay <clarklindsay96@gmail.com>
2020-01-31 17:03:44 -0500

    Can handle products that have weight, and in the even that a product that must be weighed is given without a weight, or we do not recognize the product, an error will be thrown

--- productPricing.test.ts
+++ productPricing.test.ts
@@ -4,4 +4,16 @@ describe('getting the price of products', () => {
     it('returns the price of a single valid item', () => {
         expect(getProductPrice('linguine')).toEqual(1.49);
     });
+
+    it('returns the price for an item that is priced by weight', () => {
+        expect(getProductPrice('parmesan', 0.8)).toEqual(8);
+    });
+
+    it('throws an error if the product is not carried', () => {
+        expect(() => getProductPrice('productThatDoesNotExist')).toThrow();
+    });
+
+    it('throws an error if the product is priced by weight and no weight is given', () => {
+        expect(() => getProductPrice('parmesan')).toThrow();
+    });
 });
--- productPricing.ts
+++ productPricing.ts
@@ -1,3 +1,16 @@
-export function getProductPrice(product: String) {
-    return 1.49;
+export function getProductPrice(product: string, weight: number = undefined) {
+    if (productPrices[product]) {
+        const { price, hasWeight } = productPrices[product];
+        
+        if (hasWeight && !weight) {
+            throw Error(`A weight must be given to calculate a price for ${product}`);
+        }
+        return hasWeight ? price * weight : price;
+    }
+    throw Error(`${product} is not in the product catalog`);
 }
+
+const productPrices = {
+    'linguine': { price: 1.49, hasWeight: false },
+    'parmesan': { price: 10, hasWeight: true },
+};
