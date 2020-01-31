26c029a5751aeeab98e33bbae8f12bc926028bfd
clark-lindsay <clarklindsay96@gmail.com>
2020-01-31 18:46:13 -0500

    extracted all of the product information retrieval so that it could be abstracted over a database layer without changing the business logic

--- productPricing.ts
+++ productPricing.ts
@@ -1,23 +1,38 @@
-export function getProductPrice(product: string, weight: number = undefined) {
-    if (productPrices[product]) {
-        var { price } = productPrices[product];
-        const { pricedByWeight, markdown } = productPrices[product];
+const ProductDoesNotExistError = new Error('This product is not in the product catalog');
 
-        price = applyMarkdown(price, markdown);
-        if (pricedByWeight) {
-            price = calculateWeightedPrice(price, weight);
-        }
-
-        return price;
+export function getProductPrice(product: string, weight: number = undefined) : number {
+    if (!productPrices[product]) {
+        throw ProductDoesNotExistError;
     }
-    throw Error(`${product} is not in the product catalog`);
+    var price = getUnitPrice(product);
+    const markdown = getMarkdown(product);
+    const isPricedByWeight = getIsPricedByWeight(product)
+
+    price = applyMarkdown(price, markdown);
+    if (isPricedByWeight) {
+        price = calculateWeightedPrice(price, weight);
+    }
+
+    return price;
 }
 
-function applyMarkdown(price: number, markdown: number) {
+function getUnitPrice(product: string) : number {
+    return productPrices[product].price;
+}
+
+function getMarkdown(product: string) : number {
+    return productPrices[product].markdown;
+}
+
+function applyMarkdown(price: number, markdown: number) : number {
     return markdown ? (price - markdown) : price;
 }
 
-function calculateWeightedPrice(price: number, weight: number) {
+function getIsPricedByWeight(product: string) : boolean {
+    return productPrices[product].isPricedByWeight;
+}
+
+function calculateWeightedPrice(price: number, weight: number) : number {
     if (!weight) {
         throw Error(`A weight must be given to calculate a price for a per pound product`);
     }
@@ -25,7 +40,7 @@ function calculateWeightedPrice(price: number, weight: number) {
 }
 
 const productPrices = {
-    'linguine': { price: 1.49, pricedByWeight: false },
-    'parmesan': { price: 10, pricedByWeight: true },
-    'black beans': { price: 0.89, pricedByWeight: false, markdown: 0.1 },
+    'linguine': { price: 1.49, isPricedByWeight: false },
+    'parmesan': { price: 10, isPricedByWeight: true },
+    'black beans': { price: 0.89, isPricedByWeight: false, markdown: 0.1 },
 };
