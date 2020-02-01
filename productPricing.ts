8683566cde0ab46304a7c06641a0fa8df5bbef4a
clark-lindsay <clarklindsay96@gmail.com>
2020-02-01 13:20:37 -0500

    added prettier to the project, and formatted the productPricing file

--- productPricing.ts
+++ productPricing.ts
@@ -1,46 +1,53 @@
-const ProductDoesNotExistError = new Error('This product is not in the product catalog');
+const ProductDoesNotExistError = new Error(
+  "This product is not in the product catalog"
+);
 
-export function getProductPrice(product: string, weight: number = undefined) : number {
-    if (!productPrices[product]) {
-        throw ProductDoesNotExistError;
-    }
-    var price = getUnitPrice(product);
-    const markdown = getMarkdown(product);
-    const isPricedByWeight = getIsPricedByWeight(product)
+export function getProductPrice(
+  product: string,
+  weight: number = undefined
+): number {
+  if (!productPrices[product]) {
+    throw ProductDoesNotExistError;
+  }
+  var price = getUnitPrice(product);
+  const markdown = getMarkdown(product);
+  const isPricedByWeight = getIsPricedByWeight(product);
 
-    price = applyMarkdown(price, markdown);
-    if (isPricedByWeight) {
-        price = calculateWeightedPrice(price, weight);
-    }
+  price = applyMarkdown(price, markdown);
+  if (isPricedByWeight) {
+    price = calculateWeightedPrice(price, weight);
+  }
 
-    return price;
+  return price;
 }
 
-function getUnitPrice(product: string) : number {
-    return productPrices[product].price;
+function getUnitPrice(product: string): number {
+  return productPrices[product].price;
 }
 
-function getMarkdown(product: string) : number {
-    return productPrices[product].markdown;
+function getMarkdown(product: string): number {
+  return productPrices[product].markdown;
 }
 
-function applyMarkdown(price: number, markdown: number) : number {
-    return markdown ? (price - markdown) : price;
+function applyMarkdown(price: number, markdown: number): number {
+  return markdown ? price - markdown : price;
 }
 
-function getIsPricedByWeight(product: string) : boolean {
-    return productPrices[product].isPricedByWeight;
+function getIsPricedByWeight(product: string): boolean {
+  return productPrices[product].isPricedByWeight;
 }
 
-function calculateWeightedPrice(price: number, weight: number) : number {
-    if (!weight) {
-        throw Error(`A weight must be given to calculate a price for a per pound product`);
-    }
-    return price * weight;
+function calculateWeightedPrice(price: number, weight: number): number {
+  if (!weight) {
+    throw Error(
+      `A weight must be given to calculate a price for a per pound product`
+    );
+  }
+  return price * weight;
 }
 
 const productPrices = {
-    'linguine': { price: 1.49, isPricedByWeight: false },
-    'parmesan': { price: 10, isPricedByWeight: true },
-    'black beans': { price: 0.89, isPricedByWeight: false, markdown: 0.1 },
+  linguine: { price: 1.49, isPricedByWeight: false },
+  parmesan: { price: 10, isPricedByWeight: true },
+  "black beans": { price: 0.89, isPricedByWeight: false, markdown: 0.1 }
 };
