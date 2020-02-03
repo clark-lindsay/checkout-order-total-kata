faf3c3b33078da3a312ef9498476019b5379dc7e
clark-lindsay <clarklindsay96@gmail.com>
2020-02-03 13:57:50 -0500

    Changed the way that weight is handled internally so that products are more encapsulated, and so that markdowns and weighted items interact according to the spec (markdown applied first, then multiplied by weight). Refactored the errors so that they can be reused, and take up less room when they are called.

--- productPricing.test.ts
+++ productPricing.test.ts
@@ -7,8 +7,8 @@ describe("getting the price of products", () => {
   });
 
   it("returns the price for an item that is priced by weight", () => {
-    const parmesan = getProduct("parmesan");
-    expect(parmesan.getPrice(0.8)).toEqual(8);
+    const parmesan = getProduct("parmesan", 0.8);
+    expect(parmesan.getPrice()).toEqual(8);
   });
 
   it("throws an error if the product is not carried", () => {
@@ -16,8 +16,7 @@ describe("getting the price of products", () => {
   });
 
   it("throws an error if the product is priced by weight and no weight is given", () => {
-    const parmesan = getProduct("parmesan");
-    expect(() => parmesan.getPrice()).toThrow();
+    expect(() => getProduct("parmesan")).toThrow();
   });
 
   it("supports setting markdowns", () => {
@@ -26,13 +25,20 @@ describe("getting the price of products", () => {
     expect(beans.getPrice()).toEqual(0.79);
   });
 
-  it("does not have persistent changes if we generate a new product", () => {
-    const parmesan = getProduct("parmesan");
-    parmesan.setBasePrice(5);
-    expect(parmesan.getPrice(1)).toEqual(5);
+  it("markdowns on weighted items affect the unit cost, not the final price", () => {
+    const parmesan = getProduct("parmesan", 2);
+    expect(parmesan.getPrice()).toEqual(20);
+    parmesan.setMarkdown(3);
+    expect(parmesan.getPrice()).toEqual(14);
+  });
 
-    const differentCheese = getProduct("parmesan");
-    expect(differentCheese.getPrice(1)).toEqual(10);
+  it("does not have persistent changes if we generate a new product", () => {
+    const parmesan = getProduct("parmesan", 1);
+    parmesan.setBasePrice(5);
+    expect(parmesan.getPrice()).toEqual(5);
+
+    const differentCheese = getProduct("parmesan", 1);
+    expect(differentCheese.getPrice()).toEqual(10);
   });
 
   it("supports setting prices", () => {
--- productPricing.ts
+++ productPricing.ts
@@ -1,30 +1,37 @@
-export function getProduct(name: string): Product {
+export function getProduct(name: string, weight?: number): Product {
   if (!products[name]) {
-    throw new Error(`${name} does not exist in the product catalog`);
+    throw ProductDoesNotExistError(name);
   }
-  return products[name]();
+  return products[name](weight);
 }
 
 export class Product {
   private name: string = "";
   private price: number = undefined;
   private isPricedByWeight: boolean = false;
+  private weight: number = 0;
   private markdown: number = 0;
 
-  constructor(name: string, price: number, isPricedByWeight: boolean) {
+  constructor(
+    name: string,
+    price: number,
+    isPricedByWeight: boolean,
+    weight?: number
+  ) {
+    if (isPricedByWeight && !weight) {
+      throw ProductNeedsWeightError(name);
+    }
     this.name = name;
     this.price = price;
     this.isPricedByWeight = isPricedByWeight;
+    this.weight = weight || 1;
   }
 
-  getPrice(weight?: number): number {
-    if (this.isPricedByWeight && !weight) {
-      throw Error(
-        `A weight must be given to calculate a price for a per pound product`
-      );
-    }
-    const totalPrice =
-      (weight ? this.price * weight : this.price) - this.markdown;
+  getPrice(): number {
+    const priceAfterMarkdown = this.price - this.markdown;
+    const totalPrice = this.isPricedByWeight
+      ? priceAfterMarkdown * this.weight
+      : priceAfterMarkdown;
     return totalPrice > 0 ? totalPrice : 0;
   }
 
@@ -39,6 +46,14 @@ export class Product {
 
 const products = {
   linguine: () => new Product("linquine", 1.49, false),
-  parmesan: () => new Product("parmesan", 10, true),
+  parmesan: weight => new Product("parmesan", 10, true, weight),
   "black beans": () => new Product("black beans", 0.89, false)
+};
+
+const ProductDoesNotExistError = (name?: string) =>
+  new Error(`${name || "This product"} does not exist in the product catalog.`);
+const ProductNeedsWeightError = (name?: string) => {
+  new Error(
+    `${name} is priced by weight. A weight must be given to calcultate the price of a product that is priced by weight.`
+  );
 };
