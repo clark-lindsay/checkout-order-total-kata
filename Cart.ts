3b23eebae012211c5bac957f202ee1c84f6cfe14
clark-lindsay <clarklindsay96@gmail.com>
2020-02-06 17:47:32 -0500

    extracted specials to their own classes and own file, to separate reponsibility and clean up the logic in the cart. Better Tell Don't Ask this way too

--- Cart.ts
+++ Cart.ts
@@ -1,4 +1,5 @@
 import { Product, getProduct } from './Product';
+import { NForXSpecial, BOGOSpecial } from './Specials';
 
 export class Cart {
   private contents: Product[] = [];
@@ -35,50 +36,15 @@ export class Cart {
 
   private getTotalDiscount(): number {
     let totalDiscount: number = 0;
+
     for (const special of this.nForXSpecials) {
-      totalDiscount += this.calculateNForXDiscount(special);
+      totalDiscount += special.getDiscount(this);
     }
     for (const special of this.bogoSpecials) {
-      totalDiscount += this.calculateBOGODiscount(special);
+      totalDiscount += special.getDiscount(this);
     }
 
     return totalDiscount;
-  }
-
-  private calculateNForXDiscount(special: NForXSpecial): number {
-    const numApplicableItems = this.getNumberOfItemsAffectedBySpecial(special);
-    const numDiscountApplications = Math.floor(numApplicableItems / special.productThreshold);
-
-    return getBaseCostOfAffectedItems() - getSpecialCostOfAffectedItems();
-
-    function getBaseCostOfAffectedItems(): number {
-      return numDiscountApplications * special.productThreshold * getProduct(special.productName, 1).getPrice();
-    }
-
-    function getSpecialCostOfAffectedItems(): number {
-      return numDiscountApplications * special.adjustedCost;
-    }
-  }
-
-  private calculateBOGODiscount(special: BOGOSpecial): number {
-    let numApplicableItems = this.getNumberOfItemsAffectedBySpecial(special);
-    let totalAmountOfDiscountedProduct: number = 0;
-    while (numApplicableItems > special.productThreshold) {
-      numApplicableItems -= special.productThreshold;
-      let discountedProduct: number = Math.min(special.amountOfDiscountedProduct, numApplicableItems);
-      numApplicableItems -= discountedProduct;
-      totalAmountOfDiscountedProduct += discountedProduct * special.discountPercentage;
-    }
-
-    return totalAmountOfDiscountedProduct * getProduct(special.productName, 1).getPrice();
-  }
-
-  private getNumberOfItemsAffectedBySpecial(special: BOGOSpecial | NForXSpecial): number {
-    let numAffectedItems: number = this.contains(special.productName);
-    if (special.totalProductLimit) {
-      numAffectedItems = Math.min(numAffectedItems, special.totalProductLimit);
-    }
-    return numAffectedItems;
   }
 
   // TODO: consider if there should be a generic 'addDiscount' function, which takes a special/discount object? might clarify responsibilities
@@ -102,36 +68,5 @@ export class Cart {
     this.bogoSpecials.push(
       new BOGOSpecial(productName, productThreshold, amountOfDiscountedProduct, discountPercentage, totalProductLimit)
     );
-  }
-}
-
-class NForXSpecial {
-  public productName: string;
-  public productThreshold: number;
-  public adjustedCost: number;
-  public totalProductLimit: number;
-  constructor(productName: string, productThreshold: number, adjustedCost: number, totalProductLimit?: number) {
-    this.productName = productName;
-    this.totalProductLimit = totalProductLimit;
-    this.productThreshold = productThreshold;
-    this.adjustedCost = adjustedCost;
-  }
-}
-
-class BOGOSpecial {
-  public productName: string;
-  public productThreshold: number;
-  public amountOfDiscountedProduct: number;
-  public discountPercentage: number;
-  public totalProductLimit?: number;
-  constructor(productName, productThreshold, amountOfDiscountedProduct, discountPercentage, totalProductLimit) {
-    if (discountPercentage <= 0 || discountPercentage > 1.0) {
-      throw new Error('The discount percent for a BOGO special must be in the range (0, 1]');
-    }
-    this.productName = productName;
-    this.productThreshold = productThreshold;
-    this.amountOfDiscountedProduct = amountOfDiscountedProduct;
-    this.discountPercentage = discountPercentage;
-    this.totalProductLimit = totalProductLimit;
   }
 }
